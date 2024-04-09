074f289214c3da1ce4f2a99e8caee3b7fe4e97e1
Serhii Zemov <sergzemovv78@gmail.com>
2024-04-09 17:10:46 +0300

    Added simple animations for movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,12 +7,35 @@ public class Movement : MonoBehaviour
 {
     public float speed;
 
+    public Animator animator;
+
     private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector3 direction = new Vector3(horizontal, vertical);
-        transform.position += direction * speed * Time.deltaTime;
+
+        AnimateMovement(direction);
+
+        transform.position += direction.normalized * speed * Time.deltaTime;
+    }
+
+    void AnimateMovement(Vector3 direction)
+    {
+        if (animator != null)
+        {
+            if (direction.magnitude > 0)
+            {
+                animator.SetBool("isMoving", true);
+
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
     }
 }
